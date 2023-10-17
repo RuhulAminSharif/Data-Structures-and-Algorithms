90d4dab597b6e08f6f45c2f97dcee09cad43e3c7
RuhulAminSharif <sharif.cse.ras@gmail.com>
2023-10-17 22:07:27 +0600

    (+)lower-bound

---
+++ Searching/Binary_Search/lower-bound.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define endl "\n"
+/*
+the first or the smallest index in a sorted array
+where the value at that index is greater than or 
+equal to a given key i.e. key.
+*/
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll n, key; cin >> n >> key;
+    ll a[n]; for(ll i=0; i<n; i++) cin >> a[i];
+    ll start=0, end=n-1, mid;
+    while(start<=end){
+        mid = start + (end-start)/2;
+        if(a[mid]>=key){
+            end = mid-1;
+        }else{
+            start = mid+1;
+        }
+    }
+    cout << start << endl;
+    return 0;
+}
