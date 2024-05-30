0133d9e67ed828d0c0b550b77719b3be814ab808
Ruhul Amin Sharif <sharif.cse.ras@gmail.com>
2024-05-30 07:04:01 +0600

    modified insertion sort

--- Sorting/01_InsertionSort/Insertion_Sort.cpp
+++ Sorting/01_InsertionSort/Insertion_Sort.cpp
@@ -2,24 +2,27 @@
 using namespace std;
 #define ll long long 
 #define endl "\n"
-void Insertion_Sort(ll *a, ll start, ll size)
+void insert(int arr[], int i)
 {
-    for(ll i= start; i<size; i++){
-        ll x = a[i], j=i-1;
-        while(a[j]>x && j>=0){
-            a[j+1] = a[j];
-            j--;
-        }
-        a[j+1] = x;
+    int j = i-1, x = arr[i];
+    while( j >= 0 && arr[j] > x ) {
+        arr[j+1] = arr[j];
+        j -= 1;
+    }
+    arr[j+1] = x;
+}
+void insertionSort(int arr[], int n)
+{
+    for( int i = 1; i < n; i += 1 ) {
+        insert(arr, i);
     }
 }
 int main()
 {
-    ll a[] = {3,7,0,1,5,8,3,2,34,66,87,23,12,12,12};
-    ll size = sizeof(a)/sizeof(ll);
+    int a[] = {3,7,0,1,5,8,3,2,34,66,87,23,12,12,12};
+    int size = sizeof(a)/sizeof(ll);
 
-    Insertion_Sort(a, 1, size); //a->array, (size-1)-> end index
-
+    insertionSort(a, size); 
     for(ll i=0; i<size; i++){
         cout << a[i] << " ";
     }
