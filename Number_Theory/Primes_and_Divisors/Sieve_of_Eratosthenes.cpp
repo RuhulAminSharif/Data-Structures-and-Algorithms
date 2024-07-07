d361f7a1a16230c24728c96c4319ddd6176f2f86
Ruhul Amin Sharif <sharif.cse.ras@gmail.com>
2024-07-07 23:22:40 +0600

    sieve of eratosthenes modified

--- Number_Theory/Primes_and_Divisors/Sieve_of_Eratosthenes.cpp
+++ Number_Theory/Primes_and_Divisors/Sieve_of_Eratosthenes.cpp
@@ -4,28 +4,28 @@ using namespace std;
 #define endl "\n"
 
 const ll mx = 1e8;
-vector<bool>vis(mx+1);
 vector<ll>prime;
 set<ll>prime1;
 
 void sieve()
 {
-    vis[0]=vis[1] = true;
+    vector<bool>vis(mx+1,true);
+    vis[0]=vis[1] = false;
     //all prime divisors of a number is before the square root of a number
     // 49 -> 7 prime divisors of 1 - 49  must be in 2 3 4 5 6 7
     for(ll i=3; i*i<=mx;i+=2){
-        if(vis[i]==false){
+        if( vis[i] ){
             //i*i because there 3 -> 9 if we do i+i then 6 but it has been cut down already in time of 2
             //2*i so that we can skip the even numbers
             for(ll j=i*i; j<=mx; j+=2*i){
-                vis[j]=true;
+                vis[j] = false;
             }
         }
     }
     prime.push_back(2);
     prime1.insert(2);
     for(ll i=3; i<=mx; i+=2){
-        if(vis[i]==false){
+        if( vis[i] ){
             prime.push_back(i);
             prime1.insert(i);
         }
