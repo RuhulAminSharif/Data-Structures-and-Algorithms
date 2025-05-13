8438d7f2f729a34aeaaba475e3b935e73856ee57
Sharif <sharif.cse.ras@gmail.com>
2025-05-13 23:55:03 +0600

    Updated Trie Structure

--- Data_Structure/Trie/Trie_Bits.cpp
+++ Data_Structure/Trie/Trie_Bits.cpp
@@ -4,78 +4,84 @@ using namespace std;
 #define endl "\n"
 const ll mod = 1e9+7;
 class Trie {
-private:
+  private:
     class Node {
-    public:
-        Node *nextBit[2];
+      public:
+        vector<Node*> nextBit;
+        int idx;
         Node() {
-            nextBit[0] = nextBit[1] = nullptr;
+          nextBit.resize( 2, nullptr );
+          idx = -1;
         }
     };
-public:
+  public:
     Node * root;
     static const int B = 31;
     Trie() {
-        root = new Node();
+      root = new Node();
     }
-    void insertNum( ll num ) {
-        Node *curr = root;
-        for( int i = B-1; i >= 0; i -= 1 ) {
-            int bit = ( num >> i ) & 1;
-            if( curr -> nextBit[bit] == nullptr ) {
-                curr -> nextBit[bit] = new Node();
-            }
-            curr = curr -> nextBit[bit];
+    void insertNum( ll num, ll idx ) {
+      Node *curr = root;
+      for( int i = B - 1; i >= 0; i -= 1 ) {
+        int bit = ( num >> i ) & 1;
+        if( curr -> nextBit[bit] == nullptr ) {
+          curr -> nextBit[bit] = new Node();
         }
-        return ;
+        curr = curr -> nextBit[bit];
+      }
+      curr -> idx = idx;
+      return ;
     }
-    // returns maximum of val ^ x
-    ll get_max( ll x ) {
-        Node *curr = root;
-        ll ans = 0;
-        for( int i = B-1; i >= 0; i -= 1 ) {
-            int bit = ( x >> i ) & 1;
-            if( curr -> nextBit[!bit] ) {
-                curr = curr -> nextBit[!bit];
-                ans |= ( 1 << i ) ;
-            }
-            else {
-                curr =  curr -> nextBit[bit];
-            }
+    // returns maximum of val ^ x with index -> {idx, val}
+    pair<int, ll> get_max( ll x ) {
+      Node *curr = root;
+      ll ans = 0;
+      for( int i = B - 1; i >= 0; i -= 1 ) {
+        int bit = ( x >> i ) & 1;
+        if( curr -> nextBit[!bit] ) {
+          curr = curr -> nextBit[!bit];
+          ans |= ( 1 << i ) ;
+        } else {
+          curr =  curr -> nextBit[bit];
         }
-        return ans;
+      }
+      ll idx = curr -> idx;
+      return { idx, ans } ;
     }
     // returns minimum of val ^ x
-    ll get_min( ll x ) {
-        Node *curr = root;
-        ll ans = 0;
-        for( int i = B-1; i >= 0; i -= 1 ) {
-            int bit = ( x >> i ) & 1;
-            if( curr -> nextBit[bit] ) {
-                curr = curr -> nextBit[bit];
-            }
-            else {
-                curr =  curr -> nextBit[!bit];
-                ans |= ( 1 << i ) ;
-            }
+    pair<int, ll> get_min( ll x ) {
+      Node *curr = root;
+      ll ans = 0;
+      for( int i = B-1; i >= 0; i -= 1 ) {
+        int bit = ( x >> i ) & 1;
+        if( curr -> nextBit[bit] ) {
+          curr = curr -> nextBit[bit];
+        } else {
+          curr =  curr -> nextBit[!bit];
+          ans |= ( 1 << i ) ;
         }
-        return ans;
+      }
+      ll idx = curr -> idx;
+      return { idx , ans } ;
     }
 };
 int main()
 {
-    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-    Trie *tr = new Trie();
-    int n; cin >> n;
-    vector<ll>v(n);
-    for( ll i = 0; i < n; i += 1 ) {
-        cin >> v[i];
-        tr -> insertNum(v[i]);
-    }
-    ll ans = 0;
-    for( ll i = 0; i < n; i += 1 ) {
-        ans = max( ans, tr -> get_max(v[i]) );
-    }
-    cout << ans << endl;
-    return 0;
+  ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    
+  Trie *tr = new Trie();
+  int n; cin >> n;
+  vector<ll>v(n);
+  for( ll i = 0; i < n; i += 1 ) {
+    cin >> v[i];
+    tr -> insertNum(v[i], i);
+  }
+  ll q; cin >> q;
+  for( ll t = 1; t <= q; t += 1 ) {
+    cout << "Query " << t << ": ";
+    ll x; cin >> x;
+    auto ans = tr -> get_max( x ) ;
+    cout << ans.first + 1 << " " << ans.second << endl;
+  }
+  return 0;
 }
