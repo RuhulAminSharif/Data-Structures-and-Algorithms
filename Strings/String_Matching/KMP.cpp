c8731e6f3184f748ccdcb7e7e0f58a1be8310e62
Sharif <sharif.cse.ras@gmail.com>
2025-05-14 23:06:26 +0600

    updated KMP

--- Strings/String_Matching/KMP.cpp
+++ Strings/String_Matching/KMP.cpp
@@ -2,69 +2,66 @@
 using namespace std;
 #define ll long long
 #define endl "\n"
-vector<ll> createlps( string pattern )
-{
-    ll len = pattern.size();
-    vector<ll>lps(len, 0);
-    ll idxLps = 0, idx = 1;
-    while( idx < len ) {
-        if( pattern[idxLps] == pattern[idx] ) {
-            lps[idx] = idxLps + 1;
-            idxLps += 1;
-            idx += 1;
-        }
-        else {
-            if( idxLps != 0 ) {
-                idxLps = lps[idxLps-1];
-            }
-            else {
-                lps[idx] = 0;
-                idx += 1;
-            }
-        }
-    }
-    return lps;
-}
-bool kmp(string text, string pattern){
-    
-    vector<ll> lps = createlps(pattern);
-    
-    ll idxP = 0, idxT = 0;
-    bool fg = false;
-    
-    while( idxT < text.size() ) {
-        if( pattern[idxP] == text[idxT] ) {
-            idxP += 1;
-            idxT += 1;
-        }
-        else {
-            if( idxP != 0 ) {
-                idxP = lps[idxP-1];
-            }
-            else {
-                idxT += 1;
-            }
-        }
-        if( idxP == pattern.size() ) {
-            // +1 if we need to return 1-base indexing matching starting position
-            cout << idxT - pattern.size() + 1 << endl; // from where the substring is found, starting index
-            idxP = lps[idxP-1]; //for all occurances
-            fg = true;
-            break; // if we don't require all occurance
-        }
-    }
-    if( fg ) return true;
-    else return false;
-}
-int main()
-{
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    string text, pattern;    cin  >> text >> pattern;
-    if( kmp( text, pattern ) ) {
-        cout << "Found" << endl;
-    }
-    else {  
-        cout << "Not Found" << endl;
-    }
-}
 
+vector<ll> createlps( string& pattern )
+{
+  ll len = pattern.size();
+  vector<ll> lps ( len + 1 , 0);
+  lps[0] = -1;
+  for( ll idx = 1; idx <= len; idx += 1 ) {
+    ll k = lps[idx-1];
+    while( k >= 0 && pattern[k] != pattern[idx-1] ) {
+      k = lps[k];
+    }
+    lps[idx] = k + 1;
+  }
+  return lps;
+}
+vector<ll> kmp(string& text, string& pattern){
+  vector<ll> lps = createlps(pattern);
+  vector<ll> occur;
+  ll idxP = 0, idxT = 0;
+  while( idxT < text.size() ) {
+    if( text[idxT] == pattern[idxP] ) {
+      idxP += 1;
+      idxT += 1;
+    } else {
+      if( idxP != 0 ) {
+        idxP = lps[idxP];
+      } else {
+        idxT += 1;
+      }
+    }
+    if( idxP == pattern.size() ) {
+      occur.push_back( idxT - pattern.size() + 1 );
+      idxP = lps[idxP];
+    }
+  }
+  return occur;
+}
+vector<ll> mergedKMP(string& text, string& pattern) {
+  string tt = pattern + '#' + text;
+  vector<ll> lps = createlps(tt);
+  vector<ll> occur;
+  for( ll i = pattern.size() + 1; i < lps.size(); i += 1 ) {
+    if( lps[i] == pattern.size() ) {
+      occur.push_back( i - 2 * pattern.size() );
+    }
+  }
+  return occur;
+}
+int main() {
+  string text = "AbcabcABC";
+  string pattern = "abc";
+
+  vector<ll> matches = kmp(text, pattern);
+  vector<ll> matchesC = mergedKMP(text, pattern);
+  cout << "X";
+  for (auto &idx : matches) {
+    cout << "Pattern found at 1-based index: " << idx << endl;
+  }
+  for (auto &idx : matchesC) {
+    cout << "Pattern found at 1-based index: " << idx << endl;
+  }
+  return 0;
+}
